9b1de063e61e3ae39469334795dc22523227cf86
Suneel Kumar Pal <158781115+palsuneelkm98@users.noreply.github.com>
2024-07-16 23:41:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     <div id="bodySection">
     <form onSubmit="return validate_index(this)" action="./api/login.php" method="post" name="login_page"> 
             <h2>Login</h2>
-            <input  type="text" maxlength="10" name="number" placeholder="Enter Your Mobile Number" class="no-spin"/></br></br>
-            <input type="password" name="password" placeholder="Enter Your Password"/></br></br>
+            <input  type="text" maxlength="10" name="number" placeholder="Enter Your Mobile Number" class="no-spin"/><br/><br/>
+            <input type="password" name="password" placeholder="Enter Your Password"/><br/><br/>
             <select id="dropbox" name="role">
               <option value="0">Plz Select Your Role</option>
               <option value="1">Voter </option>
               <option value="2">Group </option>
-            </select></br></br>
-            <button type="submit" id="btn">Login</button></br></br>
+            </select><br/><br/>
+            <button type="submit" id="btn">Login</button><br/><br/>
             New user? <a href="./route/register.php">Register Here</a>
             </form>
     </div>
